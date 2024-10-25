744f61493b09c91a248e197d6394208af32824e9
Chirag Vaghela <chiragvaghela10@ymail.com>
2024-10-25 23:15:57 +0200

    mulple linear regressions implemented to predict all digits.

--- ml/linear_regression.py
+++ ml/linear_regression.py
@@ -10,95 +10,99 @@ from ml.models import Model
 
 
 class LinearRegression(Model):
-    def __init__(self):
+    def __init__(self, classes=1):
         super().__init__()
+        self.classes = classes
+        self.cost_list = None
         self.w_init = None
         self.b_init = None
         self.w_min = None
         self.b_min = None
-        self.cost_list = []
 
-    def compute_gradient(self, xTrain: np.array, yTrain: np.array, w: np.array, b: np.array) -> Tuple[np.array, np.array]:
-        m = xTrain.shape[0]
-        n = xTrain.shape[1]
-        y_hat = np.zeros(m).reshape(-1, 1)
+    def compute_gradient(self, xTrain: np.array, yTrain: np.array, w: np.array,
+                         b: np.array) -> Tuple[np.array, np.array]:
+        samples = xTrain.shape[0]
+        features = xTrain.shape[1]
+        dw = np.zeros((features, self.classes))
+        db = np.zeros(self.classes)
 
-        dw, db = np.zeros((n, 10)), np.zeros(10)
-        for i in range(m):
-            y_hat[i] = np.dot(xTrain[i], w[:, 0]) + b[0]
-            dw[:, 0] += (y_hat[i] - yTrain[i, 0]) * xTrain[i]
-            db[0] += y_hat[i] - yTrain[i, 0]
+        for cls in range(self.classes):
+            y_hat = np.zeros(samples).reshape(-1, 1)
+            for sample in range(samples):
+                y_hat[sample] = np.dot(xTrain[sample], w[:, cls]) + b[cls]
+                dw[:, cls] += (y_hat[sample] - yTrain[sample, cls]) * xTrain[sample]
+                db[cls] += y_hat[sample] - yTrain[sample, cls]
 
-        dw[:, 0] = dw[:, 0] / m
-        db[0] = db[0] / m
-        return dw[:, 0], db[0]
+            dw[:, cls] = dw[:, cls] / samples
+            db[cls] = db[cls] / samples
+        return dw, db
 
     def compute_cost(self, data: np.array, target: np.array, w: np.array, b: np.array,
-                     get_y_pred: bool = False) -> [float, Tuple[np.ndarray, float]]:
-        m = data.shape[0]
-        cost = np.zeros(10)
-        #for j in range(n):
-        y_hat = np.zeros(m).reshape(-1, 1)
+                     get_y_pred: bool = False) -> [np.ndarray, Tuple[np.ndarray, np.ndarray]]:
+        samples = data.shape[0]
+        cost = np.zeros(self.classes)
+        y_hat = np.zeros((samples, self.classes))
 
-        for i in range(m):
-            y_hat[i] = np.dot(data[i], w[:, 0]) + b[0]
-            cost[0] += (y_hat[i] - target[i, 0]) ** 2
+        for cls in range(self.classes):
+            for sample in range(samples):
+                y_hat[sample, cls] = np.dot(data[sample], w[:, cls]) + b[cls]
+                cost[cls] += (y_hat[sample, cls] - target[sample, cls]) ** 2
 
-        cost[0] = cost[0] / (2 * m)
+            cost[cls] = cost[cls] / (2 * samples)
         if get_y_pred:
-            return y_hat, cost[0]
+            return y_hat, cost
         else:
-            return cost[0]
+            return cost
 
-    def plot_cost(self, cost_data: List[float]):
-        plt.plot(cost_data)
+    def gradient_descent(self, xTrain: np.array, yTrain: np.array, w_init: np.array, b_init: np.array, alpha: float,
+                         iters: int) -> Tuple[np.array, np.array, np.array]:
+        self.cost_list = np.zeros((iters, self.classes))
+        w = w_init
+        b = b_init
+
+        for i in range(iters):
+            print('Starting iteration {} ...'.format(i))
+            cost = self.compute_cost(xTrain, yTrain, w, b)
+            print('Cost after iteration {}: {}'.format(i, cost))
+            self.cost_list[i] = cost
+            dw, db = self.compute_gradient(xTrain, yTrain, w, b)
+            w_tmp = w - alpha * dw
+            b_tmp = b - alpha * db
+            w = w_tmp
+            b = b_tmp
+
+        return w, b, self.cost_list
+
+    def plot_cost(self, cost: np.array):
+        plt.plot(cost[:, 0], "-r", label="Digit 0")
+        plt.plot(cost[:, 1], "-b", label="Digit 1")
+        plt.legend(loc="upper right")
         plt.show()
 
-    def plot_result(self, pred: np.array, target: np.array, error: np.array):
+    def plot_result(self, pred: np.array, target: np.array):
         samples = len(target)
-        pred = np.round(pred)
+        pred = np.round(pred).astype(int)
         correct_pred = sum(pred[:, 0] == target[:, 0])
         print('Correct Prediction: {},  Total:{}, Accuracy: {}'.format(correct_pred,
                                                                        samples, correct_pred / samples))
-        plt.plot(pred, "-r", label='prediction')
+        plt.plot(pred[:, 0], "-r", label='prediction')
         plt.plot(target[:, 0], "-b", label='target')
         plt.legend(loc="upper right")
         plt.show()
         pass
 
-    def gradient_descent(self, xTrain: np.array, yTrain: np.array, w_init: np.array, b_init: np.array, alpha: float,
-                         iters: int) -> Tuple[np.array, np.array]:
-        w = w_init
-        b = b_init
-        for i in range(iters):
-            print('Starting iteration {} ...'.format(i))
-            cost = self.compute_cost(xTrain, yTrain, w, b)
-            print('Cost after iteration {}: {}'.format(i, cost))
-            self.cost_list.append(cost)
-            dw, db = self.compute_gradient(xTrain, yTrain, w, b)
-            w_tmp = w[:, 0] - alpha * dw
-            b_tmp = b[0] - alpha * db
-            w[:, 0] = w_tmp
-            b[0] = b_tmp
-
-        self.plot_cost(self.cost_list)
-        return w, b
-
     def train(self, xTrain: np.array, yTrain: np.array, w_init: np.array, b_init: np.array,
-              learning_rate: float, iterations: int) -> Tuple[np.array, np.array]:
+              learning_rate: float, iterations: int) -> Tuple[np.array, np.array, np.array]:
         self.w_init = w_init
         self.b_init = b_init
-        self.alpha = learning_rate
-        self.iters = iterations
 
         self.w_min, self.b_min = self.gradient_descent(xTrain, yTrain, w_init, b_init, learning_rate, iterations)
         print('Optimum weights and bias are: {}, {}'.format(self.w_min, self.b_min))
-        return self.w_min, self.b_min
+        return self.w_min, self.b_min, self.cost_list
 
-    def predict(self, xTest: np.array, yTest: np.array, wMin: np.array, bMin: float) -> np.array:
+    def predict(self, xTest: np.array, yTest: np.array, wMin: np.array, bMin: float) -> Tuple[np.array, np.array]:
         print('Predicting...')
-        y_pred, rmse = self.compute_cost(data=xTest, target=yTest, w=wMin, b=bMin, get_y_pred=True)
-        self.plot_result(y_pred, yTest, rmse)
+        return self.compute_cost(data=xTest, target=yTest, w=wMin, b=bMin, get_y_pred=True)
 
 
 class Normalizer(object):
@@ -152,7 +156,7 @@ y_train = np.repeat(np.arange(total_digits), np.round(digits_repetition * trainR
 # One hot encode y_train
 y_train = encoder.encode(y_train)
 # Create y_train with 40 repetitions of each digit in order
-y_test = np.repeat(np.arange(total_digits), np.round(digits_repetition * (1- trainRatio)).astype(int))
+y_test = np.repeat(np.arange(total_digits), np.round(digits_repetition * (1 - trainRatio)).astype(int))
 # One hot encode y_test
 y_test = encoder.encode(y_test)
 
@@ -164,17 +168,19 @@ X_test = normalizer.normalize(X_test)
 W_init = np.zeros((X_train.shape[1], total_digits))
 b_init = np.zeros(total_digits)
 
-linear_regressor = LinearRegression()
-w_min, b_min = linear_regressor.train(xTrain=X_train, yTrain=y_train, w_init=W_init, b_init=b_init,
-                                      learning_rate=1e-2, iterations=1000)
-linear_regressor.predict(xTest=X_test, yTest=y_test, wMin=w_min, bMin=b_min)
+linear_regressor = LinearRegression(total_digits)
+w_min, b_min, cost_data = linear_regressor.train(xTrain=X_train, yTrain=y_train,
+                                                 w_init=W_init, b_init=b_init, learning_rate=1e-2, iterations=1000)
+linear_regressor.plot_cost(cost=cost_data)
+y_pred, all_digits_costs = linear_regressor.predict(xTest=X_test, yTest=y_test, wMin=w_min, bMin=b_min)
+linear_regressor.plot_result(pred=y_pred, target=y_test)
 
 
-#X_train = np.arange(1, 11).reshape(-1, 1)
-#y_train = X_train ** 2
-#X_test = np.arange(11, 16).reshape(-1, 1)
-#y_test = X_test ** 2
+# X_train = np.arange(1, 11).reshape(-1, 1)
+# y_train = X_train ** 2
+# X_test = np.arange(11, 16).reshape(-1, 1)
+# y_test = X_test ** 2
 
 # Feature Engineering
-#X_train = np.c_[X_train, X_train ** 2, X_train ** 3]
-#X_test = np.c_[X_test, X_test ** 2, X_test ** 3]
+# X_train = np.c_[X_train, X_train ** 2, X_train ** 3]
+# X_test = np.c_[X_test, X_test ** 2, X_test ** 3]
